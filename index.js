0c4feeaaeec94be07d964d34ed1b3acc44bf1331
Nguyễn Huy Hoàng <hoangnguyen.hn212.hn@gmail.com>
2023-04-29 22:53:37 +0700

    .

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const app = express();
-const product = require("./api/product");
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    res.json({
+      status: 200,
+      message: "Get data has successfully",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Server error");
+  }
+});
 
 app.use(express.json({ extended: false }));
 
-app.use("/api/product", product);
+app.use("/api/product", router);
+
+app.use("", router);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
